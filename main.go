6aff9c8a91665f986f78d112ec3e326a96942660
Akeon <danakhdan12@gmail.com>
2024-01-08 16:12:38 +0700

    create address function and adjust several part

--- main.go
+++ main.go
@@ -3,15 +3,24 @@ package main
 import "fmt"
 import "rsc.io/quote"
 
-func main(){
-	fmt.Println("Hello, World !")
+func main() {
 	fullname := "Akhdan Robbani"
+	age := 20
+	town := "Nganjuk"
+	country := "Indonesia"
+
+	fmt.Println("Hello, World!!!")
 	fmt.Println(hello(fullname))
-	fmt.Println(quote.Go())
+	fmt.Println(age, "years old")
+	fmt.Println(address(town, country))
+	fmt.Println("quote rsc go :", quote.Go())
 }
 
-
-func hello(name string) string{
+func hello(name string) string {
 	message := fmt.Sprintf("%v in here", name)
 	return message
+}
+
+func address(town string, country string) string {
+	return fmt.Sprintf("lived in %v, %v", town, country)
 }
