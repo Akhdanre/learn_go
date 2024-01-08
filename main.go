f34a4823fba68c4ddc88d9135aa2acf99713d7f3
Akeon <danakhdan12@gmail.com>
2024-01-08 16:22:14 +0700

    learn variabel

--- main.go
+++ main.go
@@ -4,9 +4,13 @@ import "fmt"
 import "rsc.io/quote"
 
 func main() {
-	fullname := "Akhdan Robbani"
-	age := 20
-	town := "Nganjuk"
+	var fullname string = "Akhdan Robbani"
+	var age int8 = 20 // age := 20
+
+	var town string 
+	town = "nganjuk"
+	town = "Nganjuk"
+
 	country := "Indonesia"
 
 	fmt.Println("Hello, World!!!")
@@ -14,6 +18,7 @@ func main() {
 	fmt.Println(age, "years old")
 	fmt.Println(address(town, country))
 	fmt.Println("quote rsc go :", quote.Go())
+	fmt.Println(len("mengambil panjang string dengan len"))
 }
 
 func hello(name string) string {
