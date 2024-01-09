228115cadf4e1aba2220f4ae25174f4a5c19397a
Akeon <danakhdan12@gmail.com>
2024-01-09 17:00:52 +0700

    learn array will read value length

--- array.go
+++ array.go
@@ -7,15 +7,15 @@ func main() {
 	var names [2]string
 	names[0] = "AKhdan"
 	names[1] = "Robbani"
-
-	var values = [2]int{
-		12, 22,
+	//panjang array akan menyesuaikan pada panjang value di awal deklarasi
+	
+	var values = [...]int{
+		12, 22, 83, 192,
 	}
 
 	fmt.Println(names)
 	fmt.Println(names[0])
 	fmt.Println(names[1])
-
 
 	fmt.Println(values)
 }
