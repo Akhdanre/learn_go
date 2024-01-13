c0b91b91580ff0c46c45dabba32419923c9ff4e9
Akeon <danakhdan12@gmail.com>
2024-01-13 23:54:24 +0700

    pass by value dengan pointer

--- pointer.go
+++ pointer.go
@@ -7,10 +7,18 @@ type Address struct {
 }
 
 func main() {
+	//pass by value
 	address1 := Address{"Nganjuk", "Jatim", "indonesia"}
-	address2 := address1
+	// address2 := address1 //copy value
+	
 
-	address2.City = "Kediri"
+	//tambah & untuk pass by value dengan pointer
+	// var address2 *Address = &address1
+	address2 := &address1 //versi simple
+
+
+
+	address2.province = "Jawa Timur"
 
 	fmt.Println(address1)
 	fmt.Println(address2)
