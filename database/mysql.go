94785d6ab9349bcdee494d9d574125272f0a6030
Akeon <danakhdan12@gmail.com>
2024-01-14 16:39:47 +0700

    package Initialization

---
+++ database/mysql.go
@@ -0,0 +1,13 @@
+package database
+
+
+var connection string
+
+func init(){
+	connection = "mysql"
+}
+
+
+func GetDatabase() string {
+	return connection
+}
---
+++ init.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"learn_go/database"
+	_"learn_go/internal"
+	"fmt"
+)
+
+func main(){
+	fmt.Println(database.GetDatabase())
+}
